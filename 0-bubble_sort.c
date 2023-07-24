b012da32757b465af416e9fd81627c75eeddcf6d
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-07-24 09:17:05 -0700

    0-bubble_sort.c, 0-O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of intergers in asc  order
+ *
+ * @array: Pointer to array to be sorted
+ * @size: Size of array to be sorted
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i ,j;
+for (i = 0; i < size - 1; ++i)
+{
+for (j = 0; j < size - i - 1; ++j)
+{
+if (array[j] > array[j + 1])
+{
+int temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
+}
--- sort.h
+++ sort.h
@@ -1,11 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h> 
+#include <stdlib.h>
+
 /* print_array */
 void print_array(const int *array, size_t size);
 
-/* print_list */
-void print_list(const listint_t *list);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,7 +22,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
