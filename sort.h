ecf45eba3601919f609be95607bfadf09542ff48
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-25 06:05:48 +0000

    edited trailing spaces

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
 /* print_array */
@@ -17,9 +17,9 @@ void print_array(const int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
