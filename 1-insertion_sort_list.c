83b79ebe67b50a53257eabce5e496048d47fa3a8
John-Mary-M <johnmaryluxin8@gmail.com>
2023-07-25 18:34:26 +0000

    refactored code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,6 +7,8 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = (*list)->next;
+	listint_t *temp;
+	listint_t *sorted;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -14,30 +16,23 @@ void insertion_sort_list(listint_t **list)
 
 	while (current != NULL)
 	{
-		listint_t *temp = current->next;
-		listint_t *sorted = current->prev;
-
+		temp = current->next;
+		sorted = current->prev;
 		while (sorted != NULL && sorted->n > current->n)
 		{
 			sorted->next = current->next;
 			if (current->next != NULL)
 				current->next->prev = sorted;
-
 			current->prev = sorted->prev;
 			current->next = sorted;
 			if (sorted->prev != NULL)
 				sorted->prev->next = current;
 			else
 				*list = current;
-
 			sorted->prev = current;
 			sorted = current->prev;
-
-
 			print_list(*list);
 		}
-
 		current = temp;
 	}
-
 }
